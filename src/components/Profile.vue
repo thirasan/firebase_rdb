c47dc6ad3720364fbfb850d3410608fddf998d8f
Thirasan <thirasan@MacBook-Pro.localdomain>
2018-08-21 12:02:55 +0700

    rdb_simple

---
+++ src/components/Profile.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Email: {{this.user.email}}<br/>
+    FirstName: {{this.user.firstname}}<br/>
+    LastName: {{this.user.lastname}}<br/>
+    phoneNumber: {{this.user.phone_number}}<br/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  props: {
+    user: {
+      default: () => ({}),
+      type: Object
+    }
+  }
+}
+</script>
